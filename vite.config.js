751bbcc15f0a68a7cdb50207ecd841cada964068
yannqing <67121851@qq.com>
2023-12-24 20:41:55 +0800

    配置

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,19 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080", // 后端服务器的地址
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""), // 将请求路径中的 '/api' 前缀移除
+      },
+    },
+  },
+});
